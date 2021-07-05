8076940bda28e085fbf4d1485589ac161a3c7dbf
Varun Ramakrishnan <varun.r@hornbillfx.com>
2021-07-05 15:38:23 +0530

    Gradient triangles
    - Pass color between shaders (out and layouts)
    - Set color for each vertex of triangle
    - VAO Link method updated

--- Modern/CrashCourse/main.cpp
+++ Modern/CrashCourse/main.cpp
@@ -10,32 +10,32 @@
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	// Square coordinates
+	//               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+
+	/* // Square coordinates
 	-0.5f, -0.5f, 0.0f, // Lower left
 	0.5f, -0.5f, 0.0f, // Lower right
 	0.5f, 0.5f, 0.0f, // Upper right
-	-0.5f, 0.5f, 0.0f // Upper left
-
-	/* // Triangle coordinates
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Bottom center*/
+	-0.5f, 0.5f, 0.0f // Upper left */
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	// Square indices
-	0, 1, 3,
-	3, 1, 2
-
-	/* // Triangle indices
+	// Triangle indices
 	0, 3, 5, // Lower left triangle
 	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle*/
+	5, 4, 1 // Upper triangle
+
+	/*// Square indices
+	0, 1, 3,
+	3, 1, 2*/
 };
 
 int main()
@@ -47,7 +47,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Tell GLFW we are using the CORE profile
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "MainWindow", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1000, 1000, "MainWindow", NULL, NULL);
 
 	// Error check if the window fails to create
 	if (window == NULL)
@@ -59,7 +59,7 @@ int main()
 
 	glfwMakeContextCurrent(window); // Introduce the window to current context
 	gladLoadGL(); //Load GLAD to configure OpenGL
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, 1000, 1000);
 
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.frag", "default.vert");
@@ -71,7 +71,8 @@ int main()
 	VBO vbo1(vertices, sizeof(vertices)); // Generates Vertex Buffer Object and links it to vertices
 	EBO ebo1(indices, sizeof(indices));	// Generates Element Buffer Object and links it to indices
 
-	vao1.LinkVBO(vbo1, 0);	// Links VBO to VAO
+	vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void *)0); // Links VBO to VAO (layout == 0)
+	vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float))); // Links VBO to VAO (layout == 1)
 
 	// Unbind all to prevent accidentally modifying them
 	vao1.Unbind();
@@ -86,7 +87,7 @@ int main()
 
 		shaderProgram.Activate(); // Tell OpenGL which Shader Program we want to use
 		vao1.Bind(); // Bind the VAO so OpenGL knows to use it
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window); // Swap the back buffer with the front buffer
 		glfwPollEvents(); // Take care of all GLFW events
